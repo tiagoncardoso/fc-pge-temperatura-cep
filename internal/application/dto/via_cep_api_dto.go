685496be0e1cfea22db33bdd1a3a41a6aab5a9ca
Tiago Cardoso <tiago.nascimento@dasa.com.br>
2024-11-15 12:29:47 -0300

    chore: add return of apis dtos

---
+++ internal/application/dto/via_cep_api_dto.go
@@ -0,0 +1,17 @@
+package dto
+
+type ViaCepApiDto struct {
+	Cep         string `json:"cep"`
+	Logradouro  string `json:"logradouro"`
+	Complemento string `json:"complemento"`
+	Unidade     string `json:"unidade"`
+	Bairro      string `json:"bairro"`
+	Localidade  string `json:"localidade"`
+	Uf          string `json:"uf"`
+	Estado      string `json:"estado"`
+	Regiao      string `json:"regiao"`
+	Ibge        string `json:"ibge"`
+	Gia         string `json:"gia"`
+	Ddd         string `json:"ddd"`
+	Siafi       string `json:"siafi"`
+}
---
+++ internal/application/dto/weather_api_dto.go
@@ -0,0 +1,55 @@
+package dto
+
+type location struct {
+	Name           string  `json:"name"`
+	Region         string  `json:"region"`
+	Country        string  `json:"country"`
+	Lat            float64 `json:"lat"`
+	Lon            float64 `json:"lon"`
+	TzId           string  `json:"tz_id"`
+	LocaltimeEpoch int     `json:"localtime_epoch"`
+	Localtime      string  `json:"localtime"`
+}
+
+type current struct {
+	LastUpdatedEpoch int       `json:"last_updated_epoch"`
+	LastUpdated      string    `json:"last_updated"`
+	TempC            float64   `json:"temp_c"`
+	TempF            float64   `json:"temp_f"`
+	IsDay            int       `json:"is_day"`
+	Condition        condition `json:"condition"`
+	WindMph          float64   `json:"wind_mph"`
+	WindKph          float64   `json:"wind_kph"`
+	WindDegree       int       `json:"wind_degree"`
+	WindDir          string    `json:"wind_dir"`
+	PressureMb       int       `json:"pressure_mb"`
+	PressureIn       float64   `json:"pressure_in"`
+	PrecipMm         float64   `json:"precip_mm"`
+	PrecipIn         float64   `json:"precip_in"`
+	Humidity         int       `json:"humidity"`
+	Cloud            int       `json:"cloud"`
+	FeelslikeC       float64   `json:"feelslike_c"`
+	FeelslikeF       float64   `json:"feelslike_f"`
+	WindchillC       float64   `json:"windchill_c"`
+	WindchillF       float64   `json:"windchill_f"`
+	HeatindexC       float64   `json:"heatindex_c"`
+	HeatindexF       float64   `json:"heatindex_f"`
+	DewpointC        float64   `json:"dewpoint_c"`
+	DewpointF        float64   `json:"dewpoint_f"`
+	VisKm            int       `json:"vis_km"`
+	VisMiles         int       `json:"vis_miles"`
+	Uv               float64   `json:"uv"`
+	GustMph          float64   `json:"gust_mph"`
+	GustKph          float64   `json:"gust_kph"`
+}
+
+type condition struct {
+	Text string `json:"text"`
+	Icon string `json:"icon"`
+	Code int    `json:"code"`
+}
+
+type WeatherApiDto struct {
+	Location location `json:"location"`
+	Current  current  `json:"current"`
+}
